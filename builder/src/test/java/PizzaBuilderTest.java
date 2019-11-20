0f17cf81b688d7ffe50b3922fc72efad05c36450
meghtar <maciej.pypec@gmail.com>
2019-11-20 21:03:18 +0100

    Removed TODO comments

--- builder/src/test/java/PizzaBuilderTest.java
+++ builder/src/test/java/PizzaBuilderTest.java
@@ -3,9 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-// TODO: director
-// TODO: client
-
 public class PizzaBuilderTest {
 
     @Test
